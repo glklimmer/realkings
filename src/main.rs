8a3e8cf3a5be1b1401c9fa24d5ef06bead6e3b94
Gianluca Klimmer <pixelmannvip@gmail.com>
2024-08-26 23:10:52 +0200

    Add character controller

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use avian3d::prelude::*;
 use bevy::{
-    pbr::{CascadeShadowConfigBuilder, NotShadowCaster},
+    pbr::{
+        CascadeShadowConfigBuilder,
+        // NotShadowCaster
+    },
     prelude::*,
 };
 use bevy_third_person_camera::*;
+use bevy_tnua::prelude::*;
+use bevy_tnua_avian3d::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(ThirdPersonCameraPlugin)
+        .add_plugins((
+            TnuaControllerPlugin::default(),
+            TnuaAvian3dPlugin::default(),
+        ))
         .add_plugins(PhysicsPlugins::default())
         .add_systems(
             Startup,
             (setup_camera, setup_terrain_scene, setup_player, setup_units),
+        )
+        .add_systems(
+            Update,
+            (
+                apply_rotation,
+                apply_controls.in_set(TnuaUserControlsSystemSet),
+            ),
         )
         .run();
 }
@@ -29,7 +45,7 @@ fn setup_camera(mut commands: Commands) {
             aim_button: MouseButton::Right,
             aim_speed: 3.0,
             aim_zoom: 0.7,
-            cursor_lock_key: KeyCode::Space,
+            cursor_lock_key: KeyCode::KeyQ,
             cursor_lock_toggle_enabled: true,
             cursor_lock_active: true,
             sensitivity: Vec2::new(2.0, 2.0),
@@ -93,7 +109,7 @@ fn setup_terrain_scene(
     // });
     commands.spawn((
         RigidBody::Static,
-        Collider::cylinder(4.0, 0.1),
+        Collider::cuboid(20.0, 0.1, 20.0),
         PbrBundle {
             mesh: meshes.add(Plane3d::default().mesh().size(20., 20.)),
             material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
@@ -115,20 +131,20 @@ fn setup_terrain_scene(
     ));
 
     // Sky
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Cuboid::new(2.0, 1.0, 1.0)),
-            material: materials.add(StandardMaterial {
-                base_color: Srgba::hex("888888").unwrap().into(),
-                unlit: true,
-                cull_mode: None,
-                ..default()
-            }),
-            transform: Transform::from_scale(Vec3::splat(20.0)),
-            ..default()
-        },
-        NotShadowCaster,
-    ));
+    // commands.spawn((
+    //     PbrBundle {
+    //         mesh: meshes.add(Cuboid::new(2.0, 1.0, 1.0)),
+    //         material: materials.add(StandardMaterial {
+    //             base_color: Srgba::hex("888888").unwrap().into(),
+    //             unlit: true,
+    //             cull_mode: None,
+    //             ..default()
+    //         }),
+    //         transform: Transform::from_scale(Vec3::splat(20.0)),
+    //         ..default()
+    //     },
+    //     NotShadowCaster,
+    // ));
 }
 
 #[derive(Component)]
@@ -142,13 +158,18 @@ fn setup_player(
     // Capsule
     commands.spawn((
         PbrBundle {
-            mesh: meshes.add(Capsule3d::default()),
+            mesh: meshes.add(Cuboid::default()),
             material: materials.add(Color::hsl(237., 0.75, 0.5)),
             transform: Transform::from_xyz(5.0, 1., 0.0),
             ..default()
         },
         ThirdPersonCameraTarget,
         Player,
+        RigidBody::Dynamic,
+        Collider::capsule(0.5, 1.0),
+        TnuaControllerBundle::default(),
+        // A sensor shape is not strictly necessary, but without it we'll get weird results.
+        TnuaAvian3dSensorShape(Collider::cylinder(0.49, 0.0)),
     ));
 }
 
@@ -158,10 +179,69 @@ fn setup_units(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // Capsule
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Capsule3d::default()),
-        material: materials.add(Color::hsl(237., 0.75, 0.5)),
-        transform: Transform::from_xyz(-5.0, 1., 0.0),
-        ..default()
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Capsule3d::default()),
+            material: materials.add(Color::hsl(237., 0.75, 0.5)),
+            transform: Transform::from_xyz(-5.0, 1., 0.0),
+            ..default()
+        },
+        RigidBody::Dynamic,
+        Collider::capsule(0.5, 1.0),
+    ));
+}
+
+fn apply_rotation(
+    mut player_query: Query<&mut Transform, With<TnuaController>>,
+    cam_query: Query<&Transform, (With<Camera3d>, Without<TnuaController>)>,
+) {
+    let Ok(mut player_transform) = player_query.get_single_mut() else {
+        return;
+    };
+
+    let Ok(cam) = cam_query.get_single() else {
+        return;
+    };
+
+    let mut direction = *cam.forward();
+    direction.y = 0.0;
+    player_transform.look_to(direction, Vec3::Y);
+}
+
+fn apply_controls(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut TnuaController, &Transform)>,
+) {
+    let Ok((mut controller, transform)) = query.get_single_mut() else {
+        return;
+    };
+
+    let speed = 10.0;
+    let mut direction = Vec3::ZERO;
+
+    if keyboard.pressed(KeyCode::KeyW) {
+        direction += *transform.forward();
+    }
+    if keyboard.pressed(KeyCode::KeyS) {
+        direction += *transform.back();
+    }
+    if keyboard.pressed(KeyCode::KeyA) {
+        direction += *transform.left();
+    }
+    if keyboard.pressed(KeyCode::KeyD) {
+        direction += *transform.right();
+    }
+
+    controller.basis(TnuaBuiltinWalk {
+        desired_velocity: direction.normalize_or_zero() * speed,
+        float_height: 0.1,
+        ..Default::default()
     });
+
+    if keyboard.pressed(KeyCode::Space) {
+        controller.action(TnuaBuiltinJump {
+            height: 4.0,
+            ..Default::default()
+        });
+    }
 }
